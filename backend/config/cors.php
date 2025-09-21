5e22a9ae9b8579dcc9bfd616b8fed3a2552a24ca
Vendigo <maloy5916@inbox.ru>
2025-09-22 05:27:18 +0900

    [fix]

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,11 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_origins' => ['http://касса-крым.рф'],
+    'allowed_origins' => [
+        'http://147.45.151.90:5173',
+        'http://касса-крым.рф',
+        'http://xn----7sba2bdm1aea8h.xn--p1ai'
+    ],
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
